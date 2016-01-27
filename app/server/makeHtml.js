469948e96bb4ed0b45b195e2d8b0252a40ab5de8
Seokju Na <seokmaTD@gmail.com>
2016-01-28 05:26:28 +0900

    [app/server] make and initialize module
    'makeHtml.js' make static html files from markdown files in 'src/posts/'
    directory. Those html files are based on 'src/templates/post.ejs'.

--- app/server/makeHtml.js
+++ app/server/makeHtml.js
@@ -1,64 +1,111 @@
 var fs = require('fs');
-
 var async = require('async');
 var _ = require('underscore');
-var shortid = require('shortid');
-shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+var ejs = require('ejs');
+
+//var htmlmin = require('htmlmin');
 
 var Remarkable = require('remarkable');
 var md = new Remarkable();
 
-var blog = require('../../src/blog.json');
+const postsPath = __dirname + '/../../src/posts';
+const templatesPath = __dirname + '/../../src/templates';
+const htmlDistPath = __dirname + '/../../dist';
 
-var postsPath = __dirname + '/../../src/posts';
-var htmlDistPath = __dirname + '/../../dist/posts';
+var _postTemplateData = null;
+var _blogJSON = null;
 
 
 
-
-var _renderHtml = function(fileData) {
+/**
+ *
+ * @param fileData: Markdown File Data
+ * @return Success or Failed for write data
+ */
+var _renderHtml = function(fileName, fileData, _callback) {
     var renderedMarkdown = md.render(fileData);
 
     async.waterfall([
         function(callback) {
-            fs.readdir(htmlDistPath, function(err, files) {
-                if (err) throw err;
-                callback(null, files);
+            _getTemplateFileData(function(data) {
+                var blog = _.clone(_blogJSON);
+                delete blog['posts'];
+
+                var htmlDistData = ejs.render(data, {
+                    blog: blog,
+                    post: {
+                        title: 'Test'
+                    },
+                    renderedMarkdown: renderedMarkdown
+                });
+
+                callback(null, htmlDistData);
             });
         },
-        function(fileNames, callback) {
-            var _fileNames = _.map(fileNames, function(fileName) {
-                return (fileName.split('.'))[0];
-            });
+        function(htmlDistData, callback) {
+            fileName = fileName.split('.')[0];
 
-            var id = shortid.generate();
-
-            while (_.contains(_fileNames, id))
-                id = shortid.generate();
-
-            callback(null, id);
-        },
-        function(fileName, callback) {
-            var dataToWrite;
+            fs.writeFile(htmlDistPath + '/' + fileName + '.html',
+                htmlDistData, 'utf8',
+                function(err) {
+                    if (err) throw err;
+                    callback(null, fileName);
+                });
         }
-    ])
+    ], function(err, res) {
+        if (err) throw err;
+        console.log(res + '.html File Saved Successful!');
+        _callback(null);
+    });
+};
+
+var _getTemplateFileData = function(callback) {
+    if (_postTemplateData !== null) {
+        return callback(_postTemplateData);
+    }
+
+    fs.readFile(templatesPath + '/post.ejs', 'utf8', function(err, data) {
+        if (err) throw err;
+        _postTemplateData = data;
+        callback(_postTemplateData);
+    });
+};
+
+var _renderEachFiles = function(files, _callback) {
+    var tasks = _.map(files, function(fileName) {
+        return function(callback) {
+            var _fileName = this.fileName;
+            fs.readFile(postsPath + '/' + _fileName, 'utf8', function(err, data) {
+                _renderHtml(_fileName, data, callback);
+            });
+        }.bind({ fileName: fileName });
+    });
+
+    async.parallel(tasks, function(err, res) {
+        if (err) throw err;
+        _callback(null);
+    });
 };
 
 
-var execute = function() {
+var makeHtml = function() {
+    _blogJSON = require('../../src/blog.json');
+
     async.waterfall([
         function(callback) {
             fs.readdir(postsPath, function(err, files) {
                 if (err) throw err;
-                console.log("Getted Files", files);
                 callback(null, files);
             });
+        },
+        function(files, callback) {
+            _renderEachFiles(files, callback);
         }
-    ], function(err, results) {
-
+    ], function(err, res) {
+        if (err) throw err;
+        console.log("Done for callback hell!");
     });
-
 };
 
 
-module.exports = execute;
+module.exports = makeHtml;
