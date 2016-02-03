a1f08e8b42bde3bc7fd291eac494cd407ffe6a08
Seokju Na <seokmaTD@gmail.com>
2016-02-04 02:03:52 +0900

    [app] add node env and separate express server
    Add command line env on 'app.js' and separate express server initialzing
    codes to 'app/server/startServer.js'.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,65 @@
-var express = require('express');
-var app = express();
+#! /usr/bin/env node
 
+const async = require('async');
+const errorCodes = require('./utils/errorCodes');
+const colors = require('colors');
 
-var makeHtml = require('./server/makeHtml');
-makeHtml();
+const checkModuleInstalled = require('./utils/checkModuleInstalled');
+const checkSurgeLogin = require('./utils/checkSurgeLogin');
+
+var startServer = require('./server/startServer');
+var makeBlogJSON = require('./server/makeBlogJSON');
+
+(function() {
+    async.waterfall([
+        function(callback) {
+            console.log("[paper-press] ".green + "Check dependency module's installed.");
+            checkModuleInstalled(callback);
+        },
+
+        function(callback) {
+            console.log("[paper-press] ".green + "Check surge login.");
+            checkSurgeLogin(function(isLogin) {
+                if (!isLogin) callback(errorCodes.SURGE_LOGIN);
+                else callback(null);
+            });
+        },
+
+        function(callback) {
+            console.log("[paper-press]".green + " Check blog configure info.");
+            makeBlogJSON(function(err) {
+                if (err) callback(err);
+                else callback(null);
+            });
+        },
+
+        function(callback) {
+            console.log("[paper-press]".green + " Starting server.");
+            startServer(callback);
+        }
+    ], function(err, res) {
+        if (err) {
+            switch (err) {
+                case errorCodes.MODULE_INSTALL:
+                    console.log("[ERROR] ".red + "Module's are not installed.".red);
+                    console.log("  * Reinstall paper-press by global, ".gray + "npm install -g paper-press".bold);
+                    console.log("  * Read more guide on ".gray + "https://github.com/seokju-na/paper-press.git".underline.gray);
+                    break;
+
+                case errorCodes.SURGE_LOGIN:
+                    console.log("[ERROR] ".red + "Not currently authenticated on surge.".red);
+                    console.log("  * Login on surge. Please login surge - ".gray + "surge login".bold);
+                    console.log("  * Did you install surge by global? ".gray + "npm install -g surge".bold);
+                    console.log("  * Read more guide on ".gray + "https://github.com/seokju-na/paper-press.git".underline.gray);
+                    break;
+
+                case errorCodes.ABORTED:
+                    console.log("Aborted.");
+                    break;
+            }
+
+            console.log(" ");
+            process.exit(1);
+        }
+    });
+})();
---
+++ app/server/startServer.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const paths = require('../utils/paths');
+const colors = require('colors');
+
+var apiRoutes = require('./apiRoutes');
+var app = null;
+
+var startServer = function(callback) {
+    app = express();
+
+    app.engine('.html', require('ejs').__express);
+    app.set('views', paths.CLIENT);
+    app.set('view engine', 'ejs');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(express.static(paths.CLIENT));
+
+    app.get('/', function(req,res) {
+        res.render('index');
+    });
+
+    apiRoutes.doRoutes(app);
+
+    app.listen(8888);
+    console.log("  * Running on http://127.0.0.1:8888/ ".bold.gray + "(Press CTRL+C to quit)".grey);
+
+    callback(null);
+};
+
+module.exports = startServer;
