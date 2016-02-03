584f26d5c050a0aff57801e6494d5224fe6c9672
Seokju Na <seokmaTD@gmail.com>
2016-02-04 01:27:28 +0900

    [app/utils] make paths util
    To access to several files and libraries, make absolutely path.

---
+++ app/utils/paths.js
@@ -0,0 +1,11 @@
+module.exports = {
+    CLIENT: __dirname + '/client/dist',
+    SRC: __dirname + '/../../src/',
+    BLOG_JSON: __dirname + '/../../src/blog.config.json',
+    PAPERS: __dirname + '/../../src/papers/',
+    TEMPLATES: __dirname + '/../../src/templates/',
+    DIST: __dirname + '/../../dist/',
+
+    GULP: __dirname + '/../../node_modules/.bin/gulp',
+    SURGE: __dirname + '/../../node_modules/.bin/surge'
+};
