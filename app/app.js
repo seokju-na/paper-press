af14ee629cb01cbcc046288325fc0372f103fddd
Seokju Na <seokmaTD@gmail.com>
2016-02-10 19:36:48 +0900

    [app] modified codes for look-well
    Arrange codes.

--- app/app.js
+++ app/app.js
@@ -42,15 +42,21 @@ var makeBlogJSON = require('./server/makeBlogJSON');
             switch (err) {
                 case errorCodes.MODULE_INSTALL:
                     console.log("[ERROR] ".red + "Module's are not installed.".red);
-                    console.log("  * Reinstall paper-press by global, ".gray + "npm install -g paper-press".bold);
-                    console.log("  * Read more guide on ".gray + "https://github.com/seokju-na/paper-press.git".underline.gray);
+                    console.log("  * Reinstall paper-press by global, ".gray +
+                        "npm install -g paper-press".bold);
+                    console.log("  * Read more guide on ".gray +
+                        "https://github.com/seokju-na/paper-press.git".underline.gray);
                     break;
 
                 case errorCodes.SURGE_LOGIN:
-                    console.log("[ERROR] ".red + "Not currently authenticated on surge.".red);
-                    console.log("  * Login on surge. Please login surge - ".gray + "surge login".bold);
-                    console.log("  * Did you install surge by global? ".gray + "npm install -g surge".bold);
-                    console.log("  * Read more guide on ".gray + "https://github.com/seokju-na/paper-press.git".underline.gray);
+                    console.log("[ERROR] ".red +
+                        "Not currently authenticated on surge.".red);
+                    console.log("  * Login on surge. Please login surge - ".gray +
+                        "surge login".bold);
+                    console.log("  * Did you install surge by global? ".gray +
+                        "npm install -g surge".bold);
+                    console.log("  * Read more guide on ".gray +
+                        "https://github.com/seokju-na/paper-press.git".underline.gray);
                     break;
 
                 case errorCodes.ABORTED:
