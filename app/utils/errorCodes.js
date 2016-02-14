c925c488869786cb964d6570e04a8190b1b00d77
Seokju Na <seokmaTD@gmail.com>
2016-02-14 23:51:32 +0900

    [app/utils] modified const values
    Modified errorCodes and paths.

--- app/utils/errorCodes.js
+++ app/utils/errorCodes.js
@@ -4,13 +4,14 @@ const ErrorCodes = keyMirror({
     READ_BLOG_JSON: null,
     WRITE_BLOG_JSON: null,
     READ_DIR_OF_PAPERS: null,
+    MAKE_PAPER_FOLDER: null,
+    MAKE_DIST_FOLDER: null,
     WRITE_PAPER: null,
     WRITE_PAPER_HTML: null,
     READ_PAPER_HTML: null,
     GULP_BUILD: null,
     SURGE_DEPLOY: null,
     SURGE_LOGIN: null,
-    MODULE_INSTALL: null,
     ABORTED: null,
     UNLINK_PAPER: null
 });
--- app/utils/paths.js
+++ app/utils/paths.js
@@ -3,10 +3,13 @@ module.exports = {
     IMGS: __dirname + '/../../src/assets/imgs',
     SRC: __dirname + '/../../src/',
     BLOG_JSON: __dirname + '/../../src/blog.config.json',
+    PAPER_FOLDER: __dirname + '/../../src/papers',
     PAPERS: __dirname + '/../../src/papers/',
     TEMPLATES: __dirname + '/../../src/templates/',
     DIST: __dirname + '/../../dist/',
+    DIST_FOLDER: __dirname + '/../../dist',
 
-    GULP: __dirname + '/../../node_modules/.bin/gulp',
-    SURGE: __dirname + '/../../node_modules/.bin/surge'
+    GULP: 'gulp --gulpfile ' + __dirname + '/../../gulpfile.js',
+    SURGE: 'surge',
+    SURGE_LOCAL: __dirname + '/../../node_modules/.bin/surge'
 };
